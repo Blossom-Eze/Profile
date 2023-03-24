fcd2d5f4a565192a83a07af7047fdb6155f4cad4
‘Blossom-Eze’ <blossomeze@yahoo.com>
2023-03-24 09:50:28 +0100

    main

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Header() {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      const scrollTop = window.pageYOffset;
+      if (scrollTop > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='flex items-center justify-between text-white px-[6%] py-3 h-[10vh]'>
-      <div>
-        <h1 className='text-4xl font-bold'>Blossom.</h1>
-      </div>
-      <div className='text-xl w-1/2 px-5'>
-        <nav className='flex justify-between'>
-          <a href='#home'>Home</a>
-          <a href='#about'>About</a>
-          <a href='#services'>Services</a>
-          <a href='#skills'>Skills</a>
-          <a href='#projects'>Projects</a>
-          <a href='#contacts'>Contacts</a>
-        </nav>
+    <div  className={isScrolled ? 'scrolled' : ''}>
+      <div className='flex items-center justify-between text-white px-[6%] py-3 h-[10vh]'>
+        <div>
+          <h1 className='text-4xl font-bold'>Blossom.</h1>
+        </div>
+        <div className='text-xl w-1/2 px-5'>
+          <nav className='flex justify-between'>
+            <a href='#home'>Home</a>
+            <a href='#about'>About</a>
+            <a href='#services'>Services</a>
+            <a href='#skills'>Skills</a>
+            <a href='#projects'>Projects</a>
+            <a href='#contacts'>Contacts</a>
+          </nav>
+        </div>
       </div>
     </div>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import '../App.css';
 import Header from './Header'
 import backgroundImage from '../assets/background.jpg'
 
@@ -7,12 +7,11 @@ function Home() {
     <div className='h-screen bg-center bg-no-repeat' style={{ backgroundImage: `url(${backgroundImage})`,  backgroundSize: 'cover',
     backgroundPosition: 'center'}}>
         <Header />
-        <div className='flex flex-col justify-center text-white h-[80vh] pl-[5%]'>
-            <h3 className='text-2xl py-2'>Hello, my name is</h3>
-            <h1 className='text-6xl font-semibold'>Blossom Eze</h1>
-            <h3 className='text-2xl py-2'>I am a frontend developer</h3>
-            <h2 className='text-5xl text-purple-600'>and my tech stack is</h2>
-            <button className='bg-purple-600 w-[200px] rounded-xl text-2xl font-medium py-3 px-2'>Hire Me</button>
+        <div className='flex flex-col justify-center items-center text-white h-[80vh] pl-[5%]'>
+            <h3 className='text-2xl'>Hello, my name is</h3>
+            <h1 className='text-6xl py-1 font-semibold'>Blossom Eze</h1>
+            <h3 className='text-4xl py-2'>I am a Frontend Web Developer</h3>
+            <button className='bg-purple-600 w-[200px] rounded-xl text-2xl font-medium py-3 px-1 mt-2 bg-opacity-80'>Hire Me</button>
       </div>
     </div>
   )
